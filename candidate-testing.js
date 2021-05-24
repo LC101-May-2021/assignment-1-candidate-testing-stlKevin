bb17816ab5ed76bf48a7a0dcb7091d06ba2a758c
Kevin F <>
2021-05-24 18:52:47 +0000

    Assignment Candidate Testing Part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -12,22 +12,30 @@ let questions;
 let correctAnswers;
 let candidateAnswers;
 
+question = "Who was the first American woman in space? ";
+correctAnswer = "Sally Ride";
+candidateAnswer = "";
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (candidateAnswer === correctAnswer) {
+      console.log("That is correct!");
+  } else {
+      console.log("I'm sorry, that is incorrect.");
+  }
 
   let grade;
   
@@ -38,7 +46,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`Welcome ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
